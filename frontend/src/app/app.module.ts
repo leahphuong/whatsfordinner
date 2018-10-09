7dfb490f5538feb5807379a4448a9022333bcbb7
leahphuong <haphuong.ng@gmail.com>
2018-10-08 23:13:54 -0400

    add angular user token service to work with rails deviseauthtoken gem

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterializeModule } from 'angular2-materialize';
+import { Angular2TokenService } from 'angular2-token';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { MaterializeModule } from 'angular2-materialize';
     AppRoutingModule,
     MaterializeModule
   ],
-  providers: [],
+  providers: [Angular2TokenService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
