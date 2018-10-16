6ce2591c3d28526e6c2ec1a3a2d6305f68cf06f1
leahphuong <haphuong.ng@gmail.com>
2018-10-15 22:13:32 -0400

    create login component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { Angular2TokenService } from 'angular2-token';
 import { HttpClientModule }    from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input, EventEmitter } from '@angular/core';
+import {MaterializeAction} from "angular2-materialize";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.sass']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
