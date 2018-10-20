13e5609fc0e8b39cadaf77bb17561ce09d1d1c1a
leahphuong <haphuong.ng@gmail.com>
2018-10-19 23:45:28 -0400

    import and inject AngularTokenService into toolbar component

--- frontend/src/app/toolbar/toolbar.component.ts
+++ frontend/src/app/toolbar/toolbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Angular2TokenService} from "angular2-token";
 
 @Component({
   selector: 'app-toolbar',
@@ -7,9 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToolbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public tokenAuthService: Angular2TokenService) { }
 
   ngOnInit() {
   }
-
 }
