739ffd5fe6be0cb7038bb568babf04546220be2e
leahphuong <haphuong.ng@gmail.com>
2018-11-01 23:20:54 -0400

    add closeDialog method

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -9,11 +9,18 @@ import {MaterializeAction} from "angular2-materialize";
 export class LoginComponent implements OnInit {
   @Input('auth-mode') authMode: 'login' | 'register' = 'login';
   modalActions = new EventEmitter<string|MaterializeAction>();
+
   constructor() { }
+
   openDialog(mode: 'login' | 'register' = 'login') {
     this.authMode = mode;
     this.modalActions.emit({action:"modal", params:['open']});
   }
+
+  closeDialog(){
+    this.modalActions.emit({action:"modal", params:['close']});
+  }
+  
   ngOnInit() {
   }
   isLoginMode() {return this.authMode == 'login'};
